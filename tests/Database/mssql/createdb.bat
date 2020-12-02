dde45893955f9b4f924557b6f6814c480c1538d7
Damian Załęcki <damian.zalecki@silverbullet.pl>
2020-12-02 11:09:04 +0100

    [CHANGE] Aktualizacja skryptów tworzących i usuwających bazę danych

--- tests/Database/mssql/createdb.bat
+++ tests/Database/mssql/createdb.bat
@@ -1,21 +1,10 @@
 @echo off
 set DBNAME=SoodaUnitTests
-if (%1)==() goto usage
-set LOGIN=
-if (%2)==() set LOGIN=-E
-cls
-echo.
-echo This will create %DBNAME% database on %1
-echo.
-echo You can press Ctrl+C to quit now.
-echo.
-pause
-osql -S %1 %LOGIN% %2 %3 %4 %5 %6 -n -i createdb.sql
-pause
-goto quit
 
-:usage
-echo Usage: createdb HOSTNAME
-echo This will create %DBNAME% database on HOSTNAME
+echo Creating %DBNAME% database on %MSSQLSERVER_HOST%...
+echo.
 
-:quit
+sqlcmd -S %MSSQLSERVER_HOST% -U %MSSQLSERVER_SA_USER% -P %MSSQLSERVER_SA_PASS% -i createdb.sql
+
+echo Creating %DBNAME% database on %MSSQLSERVER_HOST%... done!
+echo.
--- tests/Database/mssql/dropdb.bat
+++ tests/Database/mssql/dropdb.bat
@@ -1,19 +1,10 @@
 @echo off
 set DBNAME=SoodaUnitTests
-if (%1)==() goto usage
-cls
-echo.
-echo This will drop %DBNAME% database on %1
-echo.
-echo You can press Ctrl+C to quit now.
-echo.
-pause
-osql -S %1 -E -n -i dropdb.sql
-pause
-goto quit
 
-:usage
-echo Usage: dropdb HOSTNAME
-echo This will drop %DBNAME% database on HOSTNAME
+echo Dropping %DBNAME% database on %MSSQLSERVER_HOST%...
+echo.
 
-:quit
+sqlcmd -S %MSSQLSERVER_HOST% -U %MSSQLSERVER_SA_USER% -P %MSSQLSERVER_SA_PASS% -i dropdb.sql
+
+echo Dropping %DBNAME% database... done!
+echo.
