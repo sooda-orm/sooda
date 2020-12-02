f2e47634d2db35376dd1bc13d4ccef7746057ed2
Damian Załęcki <damian.zalecki@silverbullet.pl>
2020-12-02 13:04:46 +0100

    [CHANGE] Drobne modyfikacjie w skryptach

--- tests/Database/mssql/createdb.bat
+++ tests/Database/mssql/createdb.bat
@@ -1,10 +1,13 @@
 @echo off
 set DBNAME=SoodaUnitTests
 
+set SCRIPT_NAME=createdb.sql
+if not (%1)==() set SCRIPT_NAME=%1\createdb.sql
+
 echo Creating %DBNAME% database on %MSSQLSERVER_HOST%...
 echo.
 
-sqlcmd -S %MSSQLSERVER_HOST% -U %MSSQLSERVER_SA_USER% -P %MSSQLSERVER_SA_PASS% -i createdb.sql
+sqlcmd -S %MSSQLSERVER_HOST% -U %MSSQLSERVER_SA_USER% -P %MSSQLSERVER_SA_PASS% -i %SCRIPT_NAME%
 
 echo Creating %DBNAME% database on %MSSQLSERVER_HOST%... done!
 echo.
--- tests/Database/mssql/dropdb.bat
+++ tests/Database/mssql/dropdb.bat
@@ -1,10 +1,14 @@
 @echo off
 set DBNAME=SoodaUnitTests
 
+set SCRIPT_NAME=createdb.sql
+if not (%1)==() set SCRIPT_NAME=%1\createdb.sql
+echo %SCRIPT_NAME%
+
 echo Dropping %DBNAME% database on %MSSQLSERVER_HOST%...
 echo.
 
-sqlcmd -S %MSSQLSERVER_HOST% -U %MSSQLSERVER_SA_USER% -P %MSSQLSERVER_SA_PASS% -i dropdb.sql
+sqlcmd -S %MSSQLSERVER_HOST% -U %MSSQLSERVER_SA_USER% -P %MSSQLSERVER_SA_PASS% -i %SCRIPT_NAME%
 
 echo Dropping %DBNAME% database... done!
 echo.
