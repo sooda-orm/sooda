4d0bd30accffd2512a6622a84cecbfeca36ea0e6
Damian Załęcki <damian.zalecki@silverbullet.pl>
2021-07-07 00:13:55 +0200

    [FUNCTION] Sooda supports .Net Core applications

---
+++ Sooda.SqlServerCore/SqlServerDbConnection.cs
@@ -0,0 +1,33 @@
+﻿using Sooda.Sql;
+using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Sooda.SqlServer
+{
+    public sealed class SqlServerDbConnection : ISoodaDbConnectionFactory
+    {
+        public IDbConnection Create()
+        {
+            var connection = new SqlConnection();
+            return connection;
+        }
+
+        public IDbConnection Create(string connectionString)
+        {
+            var connection = new SqlConnection(connectionString);
+            return connection;
+        }
+
+        public void Open(IDbConnection dbConnection)
+        {
+            dbConnection.Open();
+        }
+
+        public void Open(IDbConnection dbConnection, Action BeginTransactionAction)
+        {
+            Open(dbConnection);
+            BeginTransactionAction();
+        }
+    }
+}
